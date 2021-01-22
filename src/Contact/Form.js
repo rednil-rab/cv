9472ec9a222fea4fe3112ad44b801e41d3686fd8
Bar Linder <bar.linder@gmail.com>
2021-01-23 00:18:10 +0200

    email service

--- src/Contact/Form.js
+++ src/Contact/Form.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import { Component } from 'react';
+import emailjs from 'emailjs-com';
 
 class Form extends Component {
 
+
+    sendEmail(e) {
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+        emailjs.sendForm('service_7egxwda', 'template_lw6b5la', e.target, 'user_TCXMaNyjBwQF4ayKFjyN3')
+            .then((result) => {
+                window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
     render() {
         return (
-            <div className="bl-form">
-                <div id="nameDiv" className="input-div">
-                    <label>Name</label>
-                    <input className="top-input" type="text"></input>
+
+            <form onSubmit={this.sendEmail}>
+                <div className="bl-form">
+                    <div id="nameDiv" className="input-div">
+                        <label>Name</label>
+                        <input name="from_name" className="top-input" type="text"></input>
+                    </div>
+                    <div id="PhoneDiv" className="input-div">
+                        <label>Phone Number</label>
+                        <input name="phone" className="top-input" type="text"></input>
+                    </div>
+                    <div id="mailDiv" className="input-div">
+                        <label>E-mail</label>
+                        <input name="e_mail" className="top-input" type="text"></input>
+                    </div>
+                    <div id="subjectDiv" className="input-div">
+                        <label>Subject</label>
+                        <input name="subject" className="top-input" type="text"></input>
+                    </div>
+                    <div id="contentDiv" className="input-div">
+                        <label>Content</label>
+                        <textarea name="message" className="content-input top-input"></textarea>
+                    </div>
+                    <div id="submitDiv" className="input-div">
+                        <input type="submit" className="bl-send-btn" value="Submit"></input>
+                        
+                    </div>
                 </div>
-                <div id="PhoneDiv" className="input-div">
-                    <label>Phone Number</label>
-                    <input className="top-input" type="text"></input>
-                </div>
-                <div id="mailDiv" className="input-div">
-                    <label>E-mail</label>
-                    <input className="top-input" type="text"></input>
-                </div>
-                <div id="subjectDiv" className="input-div">
-                    <label>Subject</label>
-                    <input className="top-input" type="text"></input>
-                </div>
-                <div id="contentDiv" className="input-div">
-                    <label>Content</label>
-                    <textarea className="content-input top-input"></textarea>
-                </div>
-                <div id="submitDiv" className="input-div">
-                    <button className="bl-send-btn">submit</button>
-                </div>
-            </div>
+            </form>
+
+
         )
     }
 }
