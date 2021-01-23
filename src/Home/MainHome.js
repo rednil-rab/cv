639aa71a47fbcb95c109ff8d79e5753bcc02940a
Bar Linder <bar.linder@gmail.com>
2021-01-23 15:32:34 +0200

    heroku deploy

--- src/Home/MainHome.js
+++ src/Home/MainHome.js
@@ -17,9 +17,9 @@ class Home extends Component {
                             <h1>I'm Bar,</h1>
                             <h1>A Web developer.</h1>
                             <h3>Frontend developer / Fullstack developer.</h3>
-                            <div className="talk-btn-home">
+                            <a href="/contact"><div className="talk-btn-home">
                                 <h3>Let's talk</h3>
-                            </div>
+                            </div></a>
                         </div>
 
                     </div>
